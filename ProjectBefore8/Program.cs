5b73bdc360d4d32dbcfbd77748dbcb91efe18451
Roxana <roxana_dhz@yahoo.com>
2020-12-07 21:06:38 -0500

    My project

---
+++ ProjectBefore8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProjectBefore8
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Roxana", "Dehzad");
+            Student student1 = new Student("Roxana", "None", "Dehzad");
+            Console.WriteLine($"Welcome, {student.Name} {student.LastName}");
+            Console.WriteLine($"The Length of {student.Middle}={GetMiddleLength(student)}");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Welcome, {student1.Name} {student1.Middle} {student1.LastName}");
+            Console.WriteLine($"The Length of {student1.Middle}={GetMiddleLength(student1)}");
+
+        }
+        static int GetMiddleLength(Student student)
+        {
+            string middleName = student.Middle;
+            return middleName.Length;
+        }
+    }
+}
