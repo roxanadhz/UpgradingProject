2e0c5a1f010be399a8623dafaa68eb9f51cabf0b
Roxana <roxana_dhz@yahoo.com>
2020-12-07 20:58:26 -0500

    My project

---
+++ ProjectWithNull/Student.cs
@@ -0,0 +1,13 @@
+namespace ProjectWithNull
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Middle { get; set; }
+        public string LastName { get; set; }
+        public Student(string name, string family) => (Id, Name, LastName) = (1, name, family);
+        public Student(string name, string middle, string family) => (Id, Name, Middle, LastName) = (1, name, middle, family);       
+           
+    }
+}
